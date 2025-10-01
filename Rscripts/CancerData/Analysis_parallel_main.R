b1d4925868e27e5a7f47f474d17097d8ac27ba1e
Alessandro Colombi <colombi.alessandro@hotmail.it>
2025-10-01 12:41:00 +0200

    minor change

--- Rscripts/CancerData/Analysis_parallel_main.R
+++ Rscripts/CancerData/Analysis_parallel_main.R
@@ -1,11 +1,11 @@
-wd = "C:/Users/colom/bnp_upperbounds/Rscripts/CancerData/"
 wd = "~/Lucia/Ale/bnp_upperbounds/Rscripts/CancerData/"
+wd = "C:/Users/colom/bnp_upperbounds/Rscripts/CancerData/"
 setwd(wd)
 
 # The path to the script to execute in parallel
 scriptpath = "./Analysis_exec.R"
 Datasets = 1:32
-SimNo <- 1:16
+SimNo <- 1:6
 
 # Parallel execution 
 idx <- Datasets[SimNo[1]]; rstudioapi::jobRunScript(path = scriptpath, importEnv = TRUE)
