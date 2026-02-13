2b6720796b40b695580e57bf641debe5150a02d3
Alessandro Colombi <colombi.alessandro@hotmail.it>
2026-02-13 16:29:02 +0100

    prova batch file for running

--- Rscripts/species/Exp_WorstCase_run.R
+++ Rscripts/species/Exp_WorstCase_run.R
@@ -2,10 +2,12 @@
 wd_pc = "C:/Users/colom/"
 wd_unicatt = "C:/Users/alessandro.colombi/"
 wd_g100 = "/g100/home/userexternal/acolombi/"
-wd_vec = c(wd_pc,wd_unicatt,wd_g100)
-choose_wd = wd_vec[1] # <--- modify here
+wd_bocconi = "/home/colombi/"
+wd_vec = c(wd_pc,wd_unicatt,wd_g100,wd_bocconi)
+choose_wd = wd_vec[4] # <--- modify here
 wd = paste0(choose_wd,"bnp_upperbounds/Rscripts/species")
 setwd(wd)
+
 
 # Functions ---------------------------------------------------------------
 suppressWarnings(suppressPackageStartupMessages(library(parallel)))
