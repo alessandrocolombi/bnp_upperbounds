c951ce59c0c8a495fcaaf73fe0a5b091669f80f2
Alessandro Colombi <colombi.alessandro@hotmail.it>
2026-04-13 18:13:47 +0200

    save SS features res script before changing the notation of the files

--- Rscripts/features/SS_features_run_all.R
+++ Rscripts/features/SS_features_run_all.R
@@ -51,6 +51,47 @@ var_prior = 10
 var_fct = 100
 parallel = TRUE # <---
 
+# n fix -----------------------------------------------------------------
+n = 2000
+Mmin_grid = 100; Mmax_grid = 10000
+Mgrid = seq(Mmin_grid,Mmax_grid,by = 500); LMgrid = length(Mgrid)
+Nexp = length(Mgrid)
+seeds = sample(1:999999, size = Nexp)
+
+save_exp = TRUE # <---
+save_name_base = paste0("save/SS_features_nfix_")
+img_fld = paste0("img/")
+igrid = c(1:3) # <---
+ii = 3
+for(ii in igrid){
+  name = names(experiments)[ii]
+  Ncases = length(experiments[[ii]])
+  jj = 1
+  for(jj in 1:Ncases){
+    cat("\n ---- ",name," ",jj,"/",Ncases," ---- \n")
+    params = experiments[[ii]][[jj]]
+    trim_params = sapply(params, get_first3digits, 4)
+    if(length(trim_params)>1)
+      trim_params = paste0(trim_params[1],"_",trim_params[2])
+    
+    run_obj <- Map(function(m, s) list(M = m, seed = s),Mgrid, seeds)
+    
+    ExpRes_list = lapply( run_obj,
+                          function(x){
+                            SS_features(
+                              name=name, params=params, M=x$M, n=n,
+                              var_prior=var_prior,var_fct=var_fct,Nrep=Nrep,
+                              alpha=alpha, Rmax=Rmax, M_DM=NULL,
+                              parallel = parallel,seed0=x$seed)}  )
+    filename = paste0(save_name_base,name,"_",trim_params,".Rdat")
+    if(save_exp)
+      save(ExpRes_list,file = filename)
+  }
+}
+
+
+
+
 # M fix -----------------------------------------------------------------
 M = 5000
 Nmin_grid = 500; Nmax_grid = 10000
@@ -88,44 +129,4 @@ for(ii in igrid){
       save(ExpRes_list,file = filename)
   }
 }
-
-# n fix -----------------------------------------------------------------
-n = 2000
-Mmin_grid = 100; Mmax_grid = 10000
-Mgrid = seq(Mmin_grid,Mmax_grid,by = 500); LMgrid = length(Mgrid)
-Nexp = length(Mgrid)
-seeds = sample(1:999999, size = Nexp)
-
-save_exp = TRUE # <---
-save_name_base = paste0("save/SS_features_nfix_")
-img_fld = paste0("img/")
-igrid = c(1:3) # <---
-ii = 3
-for(ii in igrid){
-  name = names(experiments)[ii]
-  Ncases = length(experiments[[ii]])
-  jj = 1
-  for(jj in 1:Ncases){
-    cat("\n ---- ",name," ",jj,"/",Ncases," ---- \n")
-    params = experiments[[ii]][[jj]]
-    trim_params = sapply(params, get_first3digits, 4)
-    if(length(trim_params)>1)
-      trim_params = paste0(trim_params[1],"_",trim_params[2])
-
-    run_obj <- Map(function(m, s) list(M = m, seed = s),Mgrid, seeds)
-
-    ExpRes_list = lapply( run_obj,
-                          function(x){
-                            SS_features(
-                              name=name, params=params, M=x$M, n=n,
-                              var_prior=var_prior,var_fct=var_fct,Nrep=Nrep,
-                              alpha=alpha, Rmax=Rmax, M_DM=NULL,
-                              parallel = parallel,seed0=x$seed)}  )
-    filename = paste0(save_name_base,name,"_",trim_params,".Rdat")
-    if(save_exp)
-      save(ExpRes_list,file = filename)
-  }
-}
-
-
 
